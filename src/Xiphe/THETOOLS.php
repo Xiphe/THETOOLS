5cab47350718e27ed17a519fd1092722a89c68ac
Hannes Diercks <hi@xiphe.net>
2014-01-02 21:56:39 +0100

    bugfix

--- src/Xiphe/THETOOLS.php
+++ src/Xiphe/THETOOLS.php
@@ -1202,7 +1202,7 @@ class THETOOLS {
         }
         $pageURL .= "://".$_SERVER["SERVER_NAME"];
         if (isset($_SERVER["SERVER_PORT"]) && $_SERVER["SERVER_PORT"] != "80") {
-            $pageURL .= $_SERVER["SERVER_PORT"];
+            $pageURL .= ':' . $_SERVER["SERVER_PORT"];
         }
         $pageURL .= $_SERVER["REQUEST_URI"];
         return $pageURL;
